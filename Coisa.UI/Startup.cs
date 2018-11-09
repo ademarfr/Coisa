c10fca8670023d450ca58be13fb91516ca2cc550
Ademar Freitas <ademarcfreitas@gmail.com>
2018-11-09 17:21:37 -0300

    #First Commit

--- Coisa.UI/Startup.cs
+++ Coisa.UI/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+//
 namespace Coisa.UI
 {
     public class Startup
